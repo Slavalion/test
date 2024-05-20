bcfc93531442db9f3da085cb706d834bfd26047d
Kuldyaev <slavalion1@gmail.com>
2024-05-21 02:56:06 +0300

    filter obj for accounts

--- resources/js/Components/AccountsTable.vue
+++ resources/js/Components/AccountsTable.vue
@@ -1,5 +1,5 @@
 <script setup>
-defineProps({
+const props = defineProps({
     accounts: {
         type: Object,
         required: true,
@@ -9,8 +9,47 @@ defineProps({
 
 <template>
     <div class="panel w-full">
-        <div v-for="(value, name, index) in accounts" :key="index">
-            <p>{{ name }} ___{{ value }}</p>
+        <div class="line">
+            <div class="left-column">Статус</div>
+            <div class="right-column">Количество</div>
+        </div>
+        <div
+            class="line"
+            v-for="(value, name, index) in Object.fromEntries(
+                Object.entries(props.accounts).filter(([key]) => key != 'total')
+            )"
+            :key="index"
+        >
+            <div class="left-column">{{ name }}</div>
+            <div class="right-column">{{ value }}</div>
         </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.panel {
+    padding: 24px;
+    border-radius: 16px;
+}
+
+.line {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    border-top: 1px solid #e7eaf0;
+    padding-top: 12px;
+    padding-bottom: 12px;
+
+    &:first-child {
+        border-top: none;
+    }
+
+    & .left-column {
+        width: 35%;
+    }
+
+    & .rigth-column {
+        width: 65%;
+    }
+}
+</style>
